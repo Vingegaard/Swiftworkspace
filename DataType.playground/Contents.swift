c8f064f781f25ce4fc2fa3120456c2eaeb3e079d
Vingegaard <139988421+Vingegaard@users.noreply.github.com>
2023-09-05 16:09:28 -0400

    Data type

---
+++ DataType.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+
+// Bool
+
+var isChecked: Bool = false
+isChecked = true
+
+// Int
+var Numb: Int = 3000
+Numb = -2000
+
+// Uint
+var UNumb: UInt = 100
+UNumb = 100
+
+// Float
+var pi: Float = 3.14
+pi = 314
+
+// Double
+var pipi: Double = 3.1425
+pipi = 32423
+
+// Character
+var initials: Character = "A"
+initials = "Y"
+
+// String
+var string: String = "How are you?"
+string = "Good morning"
+
+//Type
+var judge = 100
+type(of: judge)
+
+var test = "Type"
+type(of: test)
+
+// Any
+var sampleAny: Any = "any"
+sampleAny = 10000
+sampleAny = 3.1425
+sampleAny = "Nono"
+
+// Nil
+var sampleInt: Int? = nil
+type(of: sampleInt)
+
+var sampleString: String? = nil
+type(of: sampleString)
+
